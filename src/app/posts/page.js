97b9c9a0882cb755ae10c2a0cdea4069a7a77f9f
Jocelyn <jocelyn.j.m.gomez@gmail.com>
2024-08-23 12:04:31 -0400

    Drawer and page jump

---
+++ src/app/posts/page.js
@@ -0,0 +1,17 @@
+import Link from "next/link";
+
+export default function page1(){
+    return (
+        <>
+        <p> This is Page1 </p>
+        <p> This is Page1 </p>
+        <p> This is Page1 </p>
+        <p> This is Page1 </p>
+        <p> This is Page1 </p>
+        <p> This is Page1 </p>
+        
+
+        <Link href="/"> Jump to Home! </Link>
+        </>
+    );
+}
