37b51792346651458ff1140b8bc95e056685bd6c
Jocelyn <jocelyn.j.m.gomez@gmail.com>
2024-09-10 14:28:53 -0400

    button onclick event

--- src/app/shapes/page.tsx
+++ src/app/shapes/page.tsx
@@ -1,22 +1,30 @@
+'use client';
 import React from 'react';
 
-
 var inputs = [
-  {"x": 175.0000000000001,"y": 177.9422863405994,"Diameter": 20.0}, 
-  {"x": 145.0000000000001,"y":177.9422863405994,"Diameter": 20.0}
+  {"x": 200.0000000000001,"y": 177.9422863405994,"Diameter": 20.0, "id":'1'}, 
+  {"x": 145.0000000000001,"y":177.9422863405994,"Diameter": 20.0, "id":'2'}
 ]
+var current
+
+
 
 const App = () => {
+  
+  const sayHello = (event) => {
+    alert(event.target.id)
+  };
+
   return (
     <>
       {inputs.map((e)=>
-        <div className="circle" 
+      <button onClick={sayHello} id={e.id}>
+        <div className='circle'
         style={{width: e.Diameter, height: e.Diameter, position: 'relative', top: e.y, left: e.x}}
         />
+      </button>
       )}   
 </>
   );
 };
-
-
 export default App;
